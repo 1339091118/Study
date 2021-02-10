ebeed154abe3dcf2fdfe922a27508a173e08dec4
autumn <c1339091118@outlook.com>
2021-02-10 17:25:43 +0800

    项目部署

---
+++ src/cn/itcast/chapter11/task02/FileClient.java
@@ -0,0 +1,25 @@
+package cn.itcast.chapter11.task02;
+
+import java.io.*;
+import java.net.*;
+
+public class FileClient {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("127.0.0.1", 10001);
+        OutputStream out = socket.getOutputStream();
+        FileInputStream fis = new FileInputStream("D:\\1.jpg");
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = fis.read(buf)) != -1) {
+            out.write(buf, 0, len);
+        }
+        socket.shutdownOutput();
+        InputStream in = socket.getInputStream();
+        byte[] bufMsg = new byte[1024];
+        int num = in.read(bufMsg);
+        String Msg = new String(bufMsg, 0, num);
+        System.out.println(Msg);
+        fis.close();
+        socket.close();
+    }
+}
